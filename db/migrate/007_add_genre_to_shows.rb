a21502eafd071455816dbaef2b1d3d3398865325
Joy Lin <vvlnote@hotmail.com>
2019-04-26 17:35:15 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_genre_to_shows.rb
+++ db/migrate/007_add_genre_to_shows.rb
@@ -1,8 +1,6 @@
-class AddShowsToNetwork < ActiveRecord::Migration[5.1]
-  #to make an additive change to a schema we create
-  #a new migration, and then in the change method,
-  #we make the change.
+class AddGenreToShows< ActiveRecord::Migration[5.1]
+
   def change
-    add_column :shows, :network_id, :integer
+    add_column :shows, :genre, :string
   end
 end
